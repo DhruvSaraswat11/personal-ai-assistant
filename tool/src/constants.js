4f28de577b6e9ff28efe803d9dbd0495a18f0ad1
Dhruv Saraswat <dhruvsaraswat22102006@gmail.com>
2025-11-25 12:47:35 +0530

    Update API key in constants.js

--- tool/src/constants.js
+++ tool/src/constants.js
@@ -1,3 +1,4 @@
 export const url =
-  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyBMlVlosQYYCVlsuYx-oARwsmab15dte5k";
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyBM0SBBgsl1UH9aJGKdkdPBnA9JCMtOmnI"
+
 
