9ae56158d8adc881556baec6c4072d4eb35ea62b
Dhruv Saraswat <dhruvsaraswat22102006@gmail.com>
2025-11-27 21:36:36 +0530

    Update constants.js

--- tool/src/constants.js
+++ tool/src/constants.js
@@ -1,5 +1,7 @@
+const key = import.meta.env.VITE_API_KEY;
 export const url =
-  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyD9Av3hnTlsFflg3prOKDUKMz6Jikw-mXs"
+  `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${key}`
+
 
 
 
